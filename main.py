65282f4bd48d9b8f06f44a8b5f325ac4383ccfaf
Elton <32944003+EltZ36@users.noreply.github.com>
2021-07-22 15:58:28 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 
 window = tk.Tk()
-welcome = tk.Label(text="Hello", fg="white", bg="black",width=10, height=10)
+welcome = tk.Button(text="Hello", fg="white", bg="black",width=10, height=10)
 welcome.pack()
 
