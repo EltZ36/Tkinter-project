8be2d861c0ee7a0e1952cc89fff045361932d217
Elton <32944003+EltZ36@users.noreply.github.com>
2021-07-22 16:15:38 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 
 window = tk.Tk()
-welcome = tk.Button(text="Hello", fg="white", bg="black",width=10, height=10)
+welcome = tk.Button(text="Hello, What is your name?", fg="white", bg="black",width=20, height=10)
+name = tk.Entry()
 welcome.pack()
+name.pack()
 
